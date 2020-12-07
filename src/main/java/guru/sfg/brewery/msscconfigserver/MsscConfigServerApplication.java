18e3b9f4715fe8c5ec8804cb3c8aa275027c4db9
Harish krishnan <harishkrishnan1993@gmail.com>
2020-12-07 22:36:43 +0530

    configurations

--- src/main/java/guru/sfg/brewery/msscconfigserver/MsscConfigServerApplication.java
+++ src/main/java/guru/sfg/brewery/msscconfigserver/MsscConfigServerApplication.java
@@ -2,8 +2,10 @@ package guru.sfg.brewery.msscconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MsscConfigServerApplication {
 
     public static void main(String[] args) {
